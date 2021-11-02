b26e5bda94a933785d3f517fc86fe4716894de4a
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-02 13:54:20 -0400

    commit tree be61b294ec4459996f537152982b11666e2e3f84

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1=2;
+let num2=31;
+const multiply =num1*num2;
+
+
+const random = Math.floor(Math.random()*10);
+
+
+let num3 =8500;
+let num4 = 36;
+const mod =num3%num4;
+
+const max = Math.max(1,5,10,15,20);
